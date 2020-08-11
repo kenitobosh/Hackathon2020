951f48761d551d589d892e5d7d0e76d3908186c3
kenitobosh <kenitobosh@gmail.com>
2020-08-10 23:00:07 -0500

    libraries

---
+++ src/main/java/Scheduling/Course.java
@@ -0,0 +1,22 @@
+package Scheduling;
+
+import java.util.List;
+
+public class Course
+{
+    int start,end;
+    String professor, building;
+    String[] date;
+    boolean online;
+
+    public Course(int start, int end, String professor, String building, String[] date, boolean online)
+    {
+        end = this.end;
+        start = this.start;
+        professor = this.professor;
+        building = this.building;
+        date = this.date;
+        online = this.online;
+    }
+
+}
--- src/main/java/Scheduling/Test.java
+++ src/main/java/Scheduling/Test.java
@@ -1,4 +1,51 @@
 package Scheduling;
 
-public class Test {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        String[] computer = {"M", "W", "F"};
+        Course computerScience = new Course(1100, 1150, "Bob","Grainger", computer, false);
+
+        String[] art = {"M", "W", "F"};
+        Course artHistory = new Course(1200, 1250, "Bob","Grainger", art, false);
+
+        Course[] courses = {artHistory, computerScience};
+
+    }
+
+    public static List<Course> res = new ArrayList<>();
+    public static List<Integer> times = new ArrayList<>();
+
+    public static List<Course> plan(Course[] load)
+    {
+        for (Course current : load)
+        {
+            if(isAvailable(current))
+            {
+                res.add(current);
+                sort(res);
+            }
+        }
+        return res;
+    }
+
+    public static boolean isAvailable(Course current)
+    {
+        if (!times.contains(current.start) && !times.contains(current.end)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static List<Course> sort(List<Course> inital)
+    {
+
+
+        return inital;
+    }
 }
+
